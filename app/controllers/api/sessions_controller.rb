243aab93d3b81a2ba426a77e70dd1b88bc04cd6f
bann-dito <ed.bacsierra@gmail.com>
2023-04-21 16:45:17 -0700

    phase 2 actually complete, previous was phas 1

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Api::SessionsController < ApplicationController
+  def show
+    @user = current_user
+    if @user
+      render json: {user: @user}
+    else
+      render json: {user: nil}
+    end
+  end
+
+  def create
+    credential = params[:credential]
+    password = params[:password]
+
+    @user = User.find_by_credentials(credential, password)
+
+    if @user
+      login!(@user)
+      render json: {user: @user}
+    else
+      render json: { errors: ['The provided credentials were invalid.'] }, 
+        status: :unauthorized
+    end
+
+  end
+
+  def destroy
+    logout!
+    render json: { message: 'success' }
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,53 @@ class ApplicationController < ActionController::API
 
 
 
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        if !logged_in?
+            render json: {errors: ["Unauthorized"]}, status: 403
+        end
+    end
+
+    def require_logged_out
+        if logged_in?
+            render json: {errors: ["Must be logged out"], status: 403}
+        end
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!
+        end
+      
+        if current_user
+          render json: { user: current_user.slice('id', 'username', 'session_token') }
+        else
+          render json: ['No current user']
+        end
+    end
+
+
+
+
     private
     def snake_case_params
         params.deep_transform_keys!(&:underscore)
