8f0a1203fe489fae9505824d3a36aa4e95e24b51
bann-dito <ed.bacsierra@gmail.com>
2023-04-23 14:04:39 -0700

    singup modal complete

--- frontend/src/components/SignupFormModal/SignUpForm.js
+++ frontend/src/components/SignupFormModal/SignUpForm.js
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
