c3aa6dbe4782d94c887826562243d4eaa3cd1651
bann-dito <ed.bacsierra@gmail.com>
2023-04-29 21:56:52 -0700

    review backend complete

---
+++ app/controllers/api/reviews_controller.rb
@@ -0,0 +1,39 @@
+class Api::ReviewsController < ApplicationController
+    before_action :require_logged_in
+    # wrap_parameters include Review.attribute_names + [:listingId]
+
+    def create
+        @review = current_user.reviews.new(review_params)
+
+        if @review.save
+            render :show
+        else
+            render json: {errors: @review.errors.full_messages}, status: :unprocessable_entity
+        end
+        
+    end
+
+
+    def destroy
+
+        @review = current_user.reviews.find(params[:id])
+
+        if @review
+            @review.destroy
+            render :show
+        else
+            render json: {message: "You don't own this review" }, status: :unauthorized
+        end
+        
+    end
+
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:listing_id, :rating, :body)
+    end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_175704) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_222312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_175704) do
     t.index ["host_id"], name: "index_listings_on_host_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "listing_id", null: false
+    t.integer "rating", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_reviews_on_author_id"
+    t.index ["listing_id", "author_id"], name: "index_reviews_on_listing_id_and_author_id", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "username", null: false
@@ -83,4 +94,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_175704) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "listings", "users", column: "host_id"
+  add_foreign_key "reviews", "listings"
+  add_foreign_key "reviews", "users", column: "author_id"
 end
